9e8036a6e5c19f93e29fde83a4c910436da0e81f
SudeshSur <sudesh.surve@npstx.com>
2024-11-23 21:02:58 +0530

    auth middleware added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 
 const express = require('express');
 const dotenv = require('dotenv').config({ path: './.env' });
+const {adminAuth , userAuth} = require('./middleware/auth-middleware')
 
 const app = express();
 
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>')
-});
+app.use('/admin', adminAuth);
+
+app.get('/user' ,  userAuth , (req, res) => {
+   res.send('user hava access')
+})
+
+app.get('/admin/getdata', (req, res) => {
+    res.send("admin data access")
+})
+
+app.get('/admin/getdata', (req, res) => {
+    res.send("admin data access")
+})
 
 app.listen(port || 3000, () => console.log(`server listing on port ${port}`)); 
---
+++ src/middleware/auth-middleware.js
@@ -0,0 +1,20 @@
+
+const  adminAuth = (req, res, next) => {
+    const token = req['headers'].authorization
+    if(!token) res.status(401).json({ error: 'unauthorized' })
+    const isAdmiinAuthrized = token.split(' ')[1] ==  process.env.ACCESS_TOKEN 
+    if (!isAdmiinAuthrized) res.status(401).json({ error: 'unauthorized' })
+    else next()
+}
+    
+
+const  userAuth = (req, res, next) => {
+    const token = req['headers'].authorization
+    if(!token) res.status(401).json({ error: 'unauthorized' })
+    const isAdmiinAuthrized = token.split(' ')[1] == process.env.ACCESS_TOKEN 
+    if (!isAdmiinAuthrized) res.status(401).json({ error: 'unauthorized' })
+    else next()
+}
+
+
+module.exports = {adminAuth , userAuth}
